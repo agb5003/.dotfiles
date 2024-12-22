73d199184149d8c715df1cdac49469f676bd6911
Maximilian Fernaldy <maximilian.fernaldy@gmail.com>
2024-12-22 17:02:17 +0900

    fresh.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,1 +1,17 @@
-../../.dotfiles/.config/nvim/init.lua
+-- Set vim language
+vim.cmd("language en_US")
+
+-- Make vim always use system clipboard
+vim.opt.clipboard = "unnamedplus"
+
+-- tabstop options
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+-- lazy.nvim
+require("config.lazy") -- this imports the settings in ~/.config/nvim/lua/config/lazy.lua
+-- don't ask me why the argument syntax looks like that. lua is fucking cursed
+
+-- 
