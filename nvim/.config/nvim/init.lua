1ec15a3166dd9192bec810e48654aaeae7d0df22
Maximilian Fernaldy <maximilian.fernaldy@gmail.com>
2024-12-24 20:33:33 +0900

    Update init.lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -4,6 +4,13 @@ require("config.lang")
 -- Make vim always use system clipboard
 vim.opt.clipboard = "unnamedplus"
 
+-- Soft wrap and wrap at words
+vim.opt.wrap = true
+vim.opt.linebreak = true
+
+-- Clear search highlight with escape
+vim.keymap.set('n', '<Esc>', ':noh<CR><Esc>')
+
 -- tabstop options
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
@@ -14,6 +21,17 @@ vim.opt.expandtab = true
 require("config.lazy") -- this imports the settings in ~/.config/nvim/lua/config/lazy.lua
 -- don't ask me why the argument syntax looks like that. lua is fucking cursed
 
+-- Keep virtual text and gutter signs visible in insert mode so the editor doesn't
+-- move around when going in and out normal and insert modes
+vim.diagnostic.config({
+    virtual_text = true,
+    signs = true,
+    update_in_insert = true,
+})
+vim.opt.signcolumn = "yes"
+
 -- Enable line numbers
 vim.opt.number = true
 
+-- Scrolloff (keep lines before and after cursor when scrolling)
+vim.opt.scrolloff = 8
