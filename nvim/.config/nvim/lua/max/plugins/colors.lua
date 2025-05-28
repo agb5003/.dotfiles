a2d04458e7953f0222569950011554a02a71b97e
Maximilian Fernaldy <maximilian.fernaldy@gmail.com>
2025-05-28 13:55:38 +0900

    nvim, waybar

--- nvim/.config/nvim/lua/max/plugins/colors.lua
+++ nvim/.config/nvim/lua/max/plugins/colors.lua
@@ -8,12 +8,12 @@ return {
     		hi CocUnusedHighlight cterm=underline gui=underline guifg=686860
     	]])
         -- Transparent background
-        vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-        vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
-        vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
-        vim.api.nvim_set_hl(0, 'Pmenu', { bg = 'none' })
-        vim.cmd([[
-            hi SignColumn guibg=NONE cterm=NONE term=NONE
-        ]])
+        -- vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+        -- vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+        -- vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
+        -- vim.api.nvim_set_hl(0, 'Pmenu', { bg = 'none' })
+        -- vim.cmd([[
+        --     hi SignColumn guibg=NONE cterm=NONE term=NONE
+        -- ]])
     end,
 }
